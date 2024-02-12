df6de433bec4bfcb74aa13915da0d83e51ca2d73
alinasiri <alinasiri576@gmail.com>
2024-02-12 14:08:25 +0330

    Fibonacci sequence

---
+++ Fibo.java
@@ -0,0 +1,18 @@
+public class Fibo {
+    public static void main(String[]args){
+        int i = 0;
+        int sum = 0;
+        int j =1;
+        int n = 8;
+        for (int k=2; k<n;++k){
+            sum = i + j;
+            System.out.printf("%d+%d = %d\n",i,j,sum);
+            i=j;
+            j=sum;
+
+
+        };
+
+    }
+
+} 
