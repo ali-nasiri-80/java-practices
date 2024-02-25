a2f81319e1cb14e1b6cfea12b9aa3bae28974d2d
Ali <95120627+ali-nasiri-80@users.noreply.github.com>
2024-02-25 12:04:21 +0330

    Update Fibo.java

--- Fibo.java
+++ Fibo.java
@@ -3,7 +3,7 @@ public class Fibo {
         int i = 0;
         int sum = 0;
         int j =1;
-        int n = 8;
+        int n = 100;
         for (int k=2; k<n;++k){
             sum = i + j;
             System.out.printf("%d+%d = %d\n",i,j,sum);
